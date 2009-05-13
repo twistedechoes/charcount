87f79b00e666c6f75d859f58c529d34a0d06fb35
rantav <rantav@cb705d06-397e-11de-9b3e-8f53452658d7>
2009-05-13 10:18:46 +0000

    Added a nice template page and some explanation to newbees

--- trunk/index.py
+++ trunk/index.py
@@ -11,7 +11,9 @@ from google.appengine.ext.webapp import template
 class MainHandler(webapp.RequestHandler):
 
   def get(self):
-    template_values = {'base_url': 'http://%s/' % self.get_host_name()}
+    template_values = {
+        'base_url': 'http://%s/' % self.get_host_name(),
+        'bookmarklet': self.get_bookmarklet_code()}
     path = os.path.join(os.path.dirname(__file__), 'index.html')
     self.response.out.write(template.render(path, template_values))
 
@@ -22,6 +24,8 @@ class MainHandler(webapp.RequestHandler):
       url = os.environ['SERVER_NAME']
     return url
 
+  def get_bookmarklet_code(self):
+    return "javascript:(function(){if(document.getElementById('__wc_display')){__wc_refresh();return};window.__wc_base='http://%s/';var d=document;var s=d.createElement('script');s.setAttribute('src',__wc_base+'s/jquery.js');s.setAttribute('type','text/javascript');d.body.appendChild(s);s=d.createElement('script');s.setAttribute('src',__wc_base+'s/wc.js');s.setAttribute('type','text/javascript');d.body.appendChild(s);})()" % (self.get_host_name())
 
 
 def main():
